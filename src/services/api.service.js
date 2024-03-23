8f35442681454fe24c823b0c383e6a3737385247
gneris177 <gneris177@gmail.com>
2024-03-23 19:52:33 -0300

    fix: ajuste

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,66 +1,74 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export class ApiService {
-  // #baseUrl = 'https://meuapetite.com/api';
-  #baseUrl = 'http://192.168.0.114:3000/api';
-
-  isAuth = false;
+  #baseUrl = process.env.REACT_APP_API_URL;
+  isAuth = false
   #token;
   #_id;
 
   constructor(routeAuth = true) {
-    if(routeAuth) {
-      this.isAuth = true;
-      this.token = localStorage.getItem('token')?.toString();
-      this._id = localStorage.getItem('_id')?.toString();
+    if (routeAuth) {
+      this.isAuth = true
+      this.token = localStorage.getItem('token')?.toString()
+      this._id = localStorage.getItem('_id')?.toString()
     }
   }
 
   getHeaders(multipart = false) {
-    const headers = {};
+    const headers = {}
 
-    if(this.isAuth) {
-      if(!this?.token || !this?._id) throw new Error('Dados de autentucação ausente!');
+    if (this.isAuth) {
+      if (!this?.token || !this?._id)
+        throw new Error('Dados de autentucação ausente!')
 
-      headers['Authorization'] = `Bearer ${this.token.replace(/"/g, '')}`;
-      headers['companyid'] = this._id.replace(/"/g, '');
+      headers['Authorization'] = `Bearer ${this.token.replace(/"/g, '')}`
+      headers['companyid'] = this._id.replace(/"/g, '')
     }
 
-    multipart ? headers['enctype'] = 'multipart/form-data' : headers['Content-Type'] = 'application/json';
+    multipart
+      ? (headers['enctype'] = 'multipart/form-data')
+      : (headers['Content-Type'] = 'application/json')
 
-    return headers;
+    return headers
   }
-
 
   verifyAuthetication(response) {
     if (response.status === 401 && this.isAuth) {
-      return window.location.href = '/login'
+      return (window.location.href = '/login')
     }
   }
 
   async get(route) {
-    const response = await axios.get(this.#baseUrl + route, { headers: this.getHeaders() });
-    this.verifyAuthetication(response);
-    return response;
+    const response = await axios.get(this.#baseUrl + route, {
+      headers: this.getHeaders(),
+    })
+    this.verifyAuthetication(response)
+    return response
   }
 
   async post(route, data, multipart = false) {
-    if(!data) throw new Error('Corpo da requisição nescessário');
-    const response = await axios.post(this.#baseUrl + route, data, { headers: this.getHeaders(multipart) });
-    this.verifyAuthetication(response);
-    return response;
+    if (!data) throw new Error('Corpo da requisição nescessário')
+    const response = await axios.post(this.#baseUrl + route, data, {
+      headers: this.getHeaders(multipart),
+    })
+    this.verifyAuthetication(response)
+    return response
   }
 
   async put(route, data, multipart = false) {
-    if(!data) throw new Error('Corpo da requisição nescessário');
-    const response = await axios.put(this.#baseUrl + route, data, { headers: this.getHeaders(multipart) });
-    this.verifyAuthetication(response);
-    return response;
+    if (!data) throw new Error('Corpo da requisição nescessário')
+    const response = await axios.put(this.#baseUrl + route, data, {
+      headers: this.getHeaders(multipart),
+    })
+    this.verifyAuthetication(response)
+    return response
   }
 
   async delete(route) {
-    const response = await axios.delete(this.#baseUrl + route, { headers: this.getHeaders() });
-    this.verifyAuthetication(response);
-    return response;
+    const response = await axios.delete(this.#baseUrl + route, {
+      headers: this.getHeaders(),
+    })
+    this.verifyAuthetication(response)
+    return response
   }
 }
